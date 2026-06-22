bfafecccf468e1801d53e7c96d45a38aed62bdea
JenithP <100717601+JenithP@users.noreply.github.com>
2026-06-22 11:54:16 +0900

    Initial Heyan booking app

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_WEB_API_KEY",
+  authDomain: "heyan-f63df.firebaseapp.com",
+  projectId: "heyan-f63df",
+  storageBucket: "heyan-f63df.firebasestorage.app",
+  messagingSenderId: "488833333270",
+  appId: "YOUR_FIREBASE_WEB_APP_ID"
+};
